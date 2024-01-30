5c1f5208e4da0381ef590485fcc2b2212aae9ebc
Ree Chen <reechendesign@gmail.com>
2024-01-29 17:29:20 -0800

    session 2 complete

---
+++ src/Pages/Routes.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+import Home from "./Home";
+import Staff from "./Staff";
+import Contact from "./Contact";
+
+const Routes = () => {
+    return (
+        <RoutesStyled>
+            Routes
+            <Home />
+        </RoutesStyled>
+    );
+};
+
+export default Routes;
+
+const RoutesStyled = styled.div``;
