8d0850fac3435ee225ec24411c6b9bb0428febe5
Ree Chen <reechendesign@gmail.com>
2024-01-29 19:30:17 -0800

    L3: Complete

--- src/Pages/Routes.jsx
+++ src/Pages/Routes.jsx
@@ -1,18 +1,44 @@
-import styled from "styled-components";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
+// global Styles
+import "@/common/css/normalize.css";
+import "@/common/css/global.scss";
+
+// layout
+import PagesLayout from "@/Common/PagesLayout/PagesLayout.jsx";
+
+// page components
 import Home from "./Home";
 import Staff from "./Staff";
 import Contact from "./Contact";
 
-const Routes = () => {
+// course work page components
+import CourseWorkLayout from "./CourseWork/CourseWorkLayout.jsx";
+import SunAndMoon from "./CourseWork/SunAndMoon/SunAndMoon.jsx";
+import Essay from "./CourseWork/Essay.jsx";
+import ResonsiveDesign from "./CourseWork/ResponsiveDesign.jsx";
+
+const MyRoutes = () => {
     return (
-        <RoutesStyled>
-            Routes
-            <Home />
-        </RoutesStyled>
+        <HashRouter>
+            <Routes>
+                <Route element={<PagesLayout />}>
+                    <Route element={<Home />} path="" />
+                    <Route element={<Staff />} path="staff" />
+                    <Route element={<Contact />} path="contact" />
+
+                    <Route element={<CourseWorkLayout />} path="course-work">
+                        <Route element={<Essay />} path="" />
+                        <Route element={<SunAndMoon />} path="sun-and-moon" />
+                        <Route
+                            element={<ResonsiveDesign />}
+                            path="responsive"
+                        />
+                    </Route>
+                </Route>
+            </Routes>
+        </HashRouter>
     );
 };
 
-export default Routes;
-
-const RoutesStyled = styled.div``;
+export default MyRoutes;
