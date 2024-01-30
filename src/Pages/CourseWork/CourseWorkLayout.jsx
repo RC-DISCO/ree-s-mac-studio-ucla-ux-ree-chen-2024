72e72ee76b6adf7560547b579084050a609c80dd
Ree Chen <reechendesign@gmail.com>
2024-01-29 19:48:33 -0800

    L3: Complete

--- src/Pages/CourseWork/CourseWorkLayout.jsx
+++ src/Pages/CourseWork/CourseWorkLayout.jsx
@@ -6,12 +6,11 @@ const CourseWorkLayout = () => {
     return (
         <CourseWorkLayoutStyled>
             <Inset>
-                {" "}
                 <h1> Course Work </h1>
                 <nav className="sublinks">
                     <NavLink to="/course-work">Essays</NavLink>
                     <NavLink to="/course-work/sun-and-moon">
-                        Sun And Moon{" "}
+                        Sun And Moon
                     </NavLink>
                     <NavLink to="/course-work/responsive">Responsive</NavLink>
                 </nav>
