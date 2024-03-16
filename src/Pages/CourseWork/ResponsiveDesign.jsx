b2852d5455613046a1992409053d04f1f6b58721
Ree Chen <reechendesign@gmail.com>
2024-03-16 22:13:47 +0100

    Final Project - 5

--- src/Pages/CourseWork/ResponsiveDesign.jsx
+++ src/Pages/CourseWork/ResponsiveDesign.jsx
@@ -8,7 +8,7 @@ const ResponsiveDesign = () => {
         <ResponsiveDesignStyled>
             <h2>Buy Gift Cards</h2>
             <div className="boxes">
-                {isMediumAndUp && <div className="box box1">$100</div>}
+                <div className="box box1">$100</div>
                 <div className="box box2">$50</div>
                 <div className="box box3">$25</div>
             </div>
@@ -28,8 +28,9 @@ const ResponsiveDesignStyled = styled.div`
     .box {
         width: 100%;
         max-width: 400px;
-        height: 200px;
-        line-height: 170px;
+        /* height: 200px; */
+        /* line-height: 170px; */
+        padding: 50px 0px;
         font-size: 50px;
         text-align: center;
         color: white;
